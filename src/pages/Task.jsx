25a05e9cedfd5d991809c747610271575719c3fe
MdMimsar <mdbx288@gmail.com>
2024-09-18 09:15:02 -0900

    Update Task.jsx

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -2,21 +2,29 @@ import "../assets/css/fontawesome.css";
 import TaskPanel from "../components/TaskPanel";
 
 const Task = () => {
+  const tasks = [
+    { heading: "Follow our community on X", reward: "250 000" },
+    { heading: "Follow our community on X", reward: "250 000" },
+    { heading: "Follow our community on X", reward: "250 000" },
+    { heading: "Follow our community on X", reward: "250 000" },
+    { heading: "Follow our community on X", reward: "250 000" },
+    { heading: "Follow our community on X", reward: "250 000" },
+    { heading: "Follow our community on X", reward: "250 000" },
+    { heading: "Follow our community on X", reward: "250 000" },
+    { heading: "Follow our community on X", reward: "250 000" },
+    { heading: "Follow our community on X", reward: "250 000" },
+  ];
+
   return (
     <>
       <div className="flex-col h-[90%] overflow-scroll no-scrollbar">
         <h2>Complete Tasks and Get Reward</h2>
         <div className="h-[80%] overflow-scroll no-scrollbar">
-          <TaskPanel heading="Follow our community on X">250 000</TaskPanel>
-          <TaskPanel heading="Follow our community on X">250 000</TaskPanel>
-          <TaskPanel heading="Follow our community on X">250 000</TaskPanel>
-          <TaskPanel heading="Follow our community on X">250 000</TaskPanel>
-          <TaskPanel heading="Follow our community on X">250 000</TaskPanel>
-          <TaskPanel heading="Follow our community on X">250 000</TaskPanel>
-          <TaskPanel heading="Follow our community on X">250 000</TaskPanel>
-          <TaskPanel heading="Follow our community on X">250 000</TaskPanel>
-          <TaskPanel heading="Follow our community on X">250 000</TaskPanel>
-          <TaskPanel heading="Follow our community on X">250 000</TaskPanel>
+          {tasks.map((task, index) => (
+            <TaskPanel key={index} heading={task.heading}>
+              {task.reward}
+            </TaskPanel>
+          ))}
         </div>
       </div>
     </>
